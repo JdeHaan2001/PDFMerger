e0dde0f51184a9c5f79fbddae3c0b9847a35b8f4
Jeroen de Haan <jeroen@totalreality.nl>
2024-10-30 14:41:20 +0100

    removed select file button

--- PDF Merger/App.xaml.cs
+++ PDF Merger/App.xaml.cs
@@ -9,6 +9,11 @@ namespace PDFMerger
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzU1NTM4MEAzMjM3MmUzMDJlMzBwQmtWNjBIVlo2UFg0VGx4Um5aSDFNOFRYK3JlN3ltZC82MHRlRU9SVGNBPQ==");
+        }
     }
 
 }
